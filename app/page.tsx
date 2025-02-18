769225f0534892bfdded60e9f4dbb61a8be9d869
rk11machaba <machabarotshidzwakaizer@gmail.com>
2025-02-18 11:21:39 +0200

    adding sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="min-h-52 min-w-80 border border-gray-300 rounded-md p-5">
-        <p>Welcome to Smart Learner Admin Port</p>
-      </div>
-    </div>
+    <main className="flex min-h-screen w-full flex-row">
+      <Sidebar/>
+    </main>
   );
 }
---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+        label: 'Dashboard',
+        route: '/',
+        imgUrl: '/icons/dashboard.svg',
+    },
+    {
+        label: 'Subject',
+        route: '/subject',
+        imgUrl: '/icons/subject.svg',
+    },
+    {
+        label: 'Video',
+        route: '/videos',
+        imgUrl: '/icons/videos.svg',
+    },
+    {
+        label: 'Question Papers',
+        route: '/papers',
+        imgUrl: '/icons/papers.svg'
+    }
+]
